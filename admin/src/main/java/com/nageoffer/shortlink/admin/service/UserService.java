b1272f0ac7a55053900c1f2b774c3dd149e9d903
Duo <1257180096@qq.com>
2024-11-16 23:51:53 +0800

    11.16 two commit

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
@@ -0,0 +1,17 @@
+package com.nageoffer.shortlink.admin.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.nageoffer.shortlink.admin.dao.entity.UserDO;
+import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
+
+/**
+ * 用户接口层
+ */
+public interface UserService extends IService<UserDO> {
+    /**
+     * 根据用户名查询用户信息
+     * @param username 用户名
+     * @return 用户信息返回类
+     */
+    UserRespDTO getUserByUsername(String username);
+}
---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.nageoffer.shortlink.admin.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.nageoffer.shortlink.admin.common.convention.exception.ClientException;
+import com.nageoffer.shortlink.admin.common.enums.UserErrorCodeEnum;
+import com.nageoffer.shortlink.admin.dao.entity.UserDO;
+import com.nageoffer.shortlink.admin.dao.mapper.UserMapper;
+import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
+import com.nageoffer.shortlink.admin.service.UserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+/**
+ * 用户接口实现层
+ */
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
+
+    @Override
+    public UserRespDTO getUserByUsername(String username) {
+        LambdaQueryWrapper<UserDO> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(UserDO::getUsername,username);
+        UserDO userDO = baseMapper.selectOne(lambdaQueryWrapper);
+        UserRespDTO userRespDTO = new UserRespDTO();
+        if(userDO!=null){
+            BeanUtils.copyProperties(userDO,userRespDTO);
+
+        }
+        throw new ClientException(UserErrorCodeEnum.USER_NULL);
+    }
+}
